389752ecf8edd5c5e841db26312ca2c5f025dc1d
unknown <1718110404@utectulancingo.edu.mx>
2020-02-19 21:12:03 -0600

    listo

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ app = web.application(urls, globals())
 render = web.template.render('templates/')
 
 
-
-##probando funciones
-
 if __name__ == "__main__":
     web.config.debug= False
     app.run()
